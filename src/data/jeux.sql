e327319c3cc3439b435043e3fa07551d896027e6
gui45 <guillaume.d.marceau@gmail.com>
2018-03-20 22:08:18 -0400

    mise a jour de la base de donne

--- src/data/jeux.sql
+++ src/data/jeux.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le :  mar. 13 mars 2018 à 18:02
+-- Généré le :  mar. 20 mars 2018 à 19:07
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.1.13
 
@@ -54,27 +54,28 @@ CREATE TABLE `jeuxvideo` (
   `ID` int(100) NOT NULL,
   `Nom` varchar(50) NOT NULL,
   `description` varchar(1000) NOT NULL,
-  `idGenre` int(11) NOT NULL
+  `idGenre` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `jeuxvideo`
 --
 
-INSERT INTO `jeuxvideo` (`ID`, `Nom`, `description`, `idGenre`) VALUES
-(1, 'Eve online', 'eve est un mmo rpg baser dans le futur ', 1),
-(2, 'world of warcraft ', 'word of warcraft est un mmo rpg', 1),
-(3, 'Battlefield 1', 'battlefield 1 est un fps de la ww1', 2),
-(4, 'the elder scrolls online', 'TESO est une mmo rpg dans l\'univer de elder scrolls', 1),
-(5, 'call of duty', 'cod est un fps', 2),
-(7, 'counter strike', 'un jeu fps competitif multijoueur', 2),
-(8, 'half life 2', 'un fps dont la trilogie ne ce conclura jamais', 2),
-(9, 'world of tanks', 'un jeu mmo / moba de tank', 1),
-(10, 'warthunder', 'une simulation mmo / moba de vehicule de guerre', 1),
-(15, 'League of Legends', 'un moba', 3),
-(16, 'Skyrim', 'un RPG', 4),
-(17, 'Fallout 4', 'un rpg', 4),
-(18, 'Dota', 'un moba', 3);
+INSERT INTO `jeuxvideo` (`ID`, `Nom`, `description`, `idGenre`, `image`) VALUES
+(1, 'Eve online', 'eve est un mmo rpg baser dans le futur ', 1, '../illustration/miniatureblue.png'),
+(2, 'world of warcraft ', 'word of warcraft est un mmo rpg', 1, ''),
+(3, 'Battlefield 1', 'battlefield 1 est un fps de la ww1', 2, ''),
+(4, 'the elder scrolls online', 'TESO est une mmo rpg dans l\'univer de elder scrolls', 1, ''),
+(5, 'call of duty', 'cod est un fps', 2, ''),
+(7, 'counter strike', 'un jeu fps competitif multijoueur', 2, ''),
+(8, 'half life 2', 'un fps dont la trilogie ne ce conclura jamais', 2, '../illustration/miniaturebase.png'),
+(9, 'world of tanks', 'un jeu mmo / moba de tank', 1, '../illustration/miniaturealecto.png'),
+(10, 'warthunder', 'une simulation mmo / moba de vehicule de guerre', 1, ''),
+(15, 'League of Legends', 'un moba', 3, ''),
+(16, 'Skyrim', 'un RPG', 4, ''),
+(17, 'Fallout 4', 'un rpg', 4, ''),
+(18, 'Dota', 'un moba', 3, '');
 
 --
 -- Index pour les tables déchargées
@@ -101,13 +102,13 @@ ALTER TABLE `jeuxvideo`
 -- AUTO_INCREMENT pour la table `genre`
 --
 ALTER TABLE `genre`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `jeuxvideo`
 --
 ALTER TABLE `jeuxvideo`
-  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
