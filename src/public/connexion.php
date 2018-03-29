7af0f36b0039552dfcc9e058bbb048dddf9fa3ab
gui45 <guillaume.d.marceau@gmail.com>
2018-03-29 14:30:35 -0400

    ajout  du formulaire inscription

---
+++ src/public/connexion.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<section id="">
+		<h1>Projet Liste</h1>
+		<h2>Inscription</h2>
+		<form method="POST" action="actionConnexion.php">
+      <div>
+        <label for="motDePasse">mot De Passe</label>
+        <input type="password" name="motDePasse" id="motDePasse">
+      </div>
+  		<div>
+        <label for="prenom">prenom</label>
+        <input type="text" name="prenom" id="prenom">
+  		</div>
+  			<input type="submit" value="soumettre">
+    </form>
+	</section>
+	<a href="../accueil.html">retour</a>
+</body>
+</html>
