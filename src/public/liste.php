f85ef22fe2c4d74252f6b1bca30ef5555978c468
gui45 <guillaume.d.marceau@gmail.com>
2018-04-11 16:34:29 -0400

    exteriorisation

--- src/public/liste.php
+++ src/public/liste.php
@@ -8,35 +8,8 @@
 ?>
 <html lang="fr">
 	<script type="text/javascript" src="../lib/Ajax.js"></script>
+	<script type="text/javascript" src="afficherListe.js"></script>
 	<head>
-		<script>
-		var precedent = null;
-		var courant = null;
-			function appel(genre)
-			{
-				//console.log(genre);
-				if(null != precedent)precedent.innerHTML = "";
-				courrant = genre;
-				ajax = new Ajax();
-				//console.log(ajax);
-				ajax.executer("GET", "http://localhost/projet-web-php-2018-gui45/src/dao/serviceWeb.php", "", afficher)
-				precedent = genre;
-			}
-			function afficher(ajax)
-			{
-				//console.log(ajax.responseXML);
-				var html = "";
-				xmlGenre = ajax.responseXML.getElementsByTagName(courrant.id)[0];
-				xmlListeJeux = xmlGenre.getElementsByTagName('jeu');
-				for (var i = 0; i < xmlListeJeux.length; i++) {
-					//console.log(xmlListeJeux[i]);
-					nom = xmlListeJeux[i].getElementsByTagName('nom')[0].innerHTML;
-					id = xmlListeJeux[i].getElementsByTagName('id')[0].innerHTML;
-					html += "<a href='detail.php?id=" + id + "'>" + nom + "</a>"
-				}
-				courrant.innerHTML = html;
-			}
-		</script>
 		<meta charset="utf-8">
 		<title>liste jeux</title>
 	</head>
