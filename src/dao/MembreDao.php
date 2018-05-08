f4eba66e03dddaa521eabee626c3db32a6983db3
gui45 <guillaume.d.marceau@gmail.com>
2018-05-07 22:02:31 -0400

    bouger les fonctions membre

--- src/dao/MembreDao.php
+++ src/dao/MembreDao.php
@@ -7,5 +7,37 @@ else
   $local = 'fr';
 class MembreDao
 {
+  function ajouterMembre($membre)//connection
+	{
+		global $basededonnees;
+		$SQL_AJOUTER_MEMBRE = "INSERT INTO membre(prenom, nom, couriel, telephone, adresse, motDePasse, sexe) VALUES(:prenom, :nom, :couriel, :telephone, :adresse, :motDePasse, :sexe)";
+		$requete = $basededonnees->prepare($SQL_AJOUTER_MEMBRE);
+		$requete->bindParam(':prenom', $membre['prenom'], PDO::PARAM_STR);
+		$requete->bindParam(':nom', $membre['nom'], PDO::PARAM_STR);
+		$requete->bindParam(':couriel', $membre['couriel'], PDO::PARAM_STR);
+		$requete->bindParam(':telephone', $membre['telephone'], PDO::PARAM_STR);
+		$requete->bindParam(':adresse', $membre['adresse'], PDO::PARAM_STR);
+		$requete->bindParam(':motDePasse', $membre['motDePasse'], PDO::PARAM_STR);
+		$requete->bindParam(':sexe', $membre['sexe'], PDO::PARAM_STR);
+		$requete->execute();
+	}
+	function trouverMembre($membre)//connecion
+	{
+		global $basededonnees;
+		$SQL_TROUVER_MEMBRE = "SELECT * FROM membre WHERE prenom = :prenom";
+		$requete = $basededonnees->prepare($SQL_TROUVER_MEMBRE);
+		$requete->bindParam(':prenom', $membre['prenom'], PDO::PARAM_STR);
+		$requete->execute();
+	  $membreTrouve =$requete->fetch();
+		if(strcmp($membre["motDePasse"],$membreTrouve["motDePasse"]) == 0)
+		{
+			session_start();
+			$_SESSION['membre'] = $membreTrouve;
+			//print_r($_SESSION['membre']);
+		}
+		else {
+			echo("erreur avec le mot de passe ou le prenom");
+		}
+	}
 }
 ?>
