a248a1a8fe7f1953fb270487435005c4842fd14c
gui45 <guillaume.d.marceau@gmail.com>
2018-05-07 22:03:34 -0400

    ajuster dao membre

--- src/dao/MembreDao.php
+++ src/dao/MembreDao.php
@@ -1,10 +1,5 @@
 <?php
 include_once "baseDeDonner.php";
-$langue = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-if($langue == 'en' || $langue == 'fr')
-  $local = $langue;
-else
-  $local = 'fr';
 class MembreDao
 {
   function ajouterMembre($membre)//connection
