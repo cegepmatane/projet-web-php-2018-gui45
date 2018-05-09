e8b00c57e65efc3f8d8d6f33d33ffb346fe862c0
gui45 <guillaume.d.marceau@gmail.com>
2018-05-09 15:29:42 -0400

    liste des genre dans le excel

--- src/export/excel.php
+++ src/export/excel.php
@@ -1,14 +1,22 @@
 <?php
+include_once '../dao/JeuDao.php';
+include_once '../dao/GenreDao.php';
+$genreDao = new GenreDao;
+$jeuDao = new JeuDao;
+
 require 'C:\Users\gui\vendor\autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $document = new spreadsheet();
 $feuille = $document->getActiveSheet();
 
-$document = new Spreadsheet();
-$feuille = $document->getActiveSheet();
-$feuille->setCellValue('A1', 'Hello World !');
+$genres= $genreDao->lireGenres();
+
+foreach($genres as $genre){
+  $feuille->setCellValue('A'. $genre['ID'], $genre['Nom']);
+}
+
 
 $imprimeur = new Xlsx($document);
-$imprimeur->save('test.xlsx');
+$imprimeur->save('liste.xlsx');
 ?>
