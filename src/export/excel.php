caefabd15dac6665b0d0bd4e2d41b67fd5b2d000
gui45 <guillaume.d.marceau@gmail.com>
2018-05-15 14:23:26 -0400

    le script de genertion excel ne ce genrer plus si le fichier existe deja

--- src/export/excel.php
+++ src/export/excel.php
@@ -5,30 +5,33 @@ $genreDao = new GenreDao;
 require 'C:\Users\gui\vendor\autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-$document = new spreadsheet();
-$feuille = $document->getActiveSheet();
-
-$genres= $genreDao->lireGenres();
-
-foreach($genres as $genre){
-  $feuille->setCellValue('A'. $genre['ID'], $genre['Nom']);
-}
-
 $fichier = 'liste.xlsx';
-$imprimeur = new Xlsx($document);
-$imprimeur->save($fichier);
+if(!file_exists($fichier)){
+  $document = new spreadsheet();
+  $feuille = $document->getActiveSheet();
 
-if (file_exists($fichier)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($fichier).'"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($fichier));
-    readfile($fichier);
-    exit;
+  $genres= $genreDao->lireGenres();
+
+  foreach($genres as $genre){
+    $feuille->setCellValue('A'. $genre['ID'], $genre['Nom']);
+  }
+
+
+  $imprimeur = new Xlsx($document);
+  $imprimeur->save($fichier);
+
+  if (file_exists($fichier)) {
+      header('Content-Description: File Transfer');
+      header('Content-Type: application/octet-stream');
+      header('Content-Disposition: attachment; filename="'.basename($fichier).'"');
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      header('Content-Length: ' . filesize($fichier));
+      readfile($fichier);
+      exit;
+  }
+  header("location: http://localhost/projet-web-php-2018-gui45/src/public/liste.php");
+  die();
 }
-header("location: http://localhost/projet-web-php-2018-gui45/src/public/liste.php");
-die();
 ?>
